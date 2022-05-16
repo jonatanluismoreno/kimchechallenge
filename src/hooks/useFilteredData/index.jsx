742c760404b812e5c494abda8719acd6c414062a
Jonatan Luis Moreno <jonatanluismoreno@gmail.com>
2022-05-16 09:46:56 -0300

    add custom hook to filter input data

---
+++ src/hooks/useFilteredData/index.jsx
@@ -0,0 +1,79 @@
+import { useMemo } from "react";
+import { gql, useQuery } from "@apollo/client";
+const LIST_COUNTRIES = gql`
+  {
+    countries {
+      name
+      code
+      continent {
+        name
+      }
+      languages {
+        name
+        code
+      }
+    }
+  }
+`;
+
+const LIST_LANGUAGES = gql`
+  {
+    languages {
+      name
+    }
+  }
+`;
+const LIST_CONTINENTS = gql`
+  {
+    continents {
+      name
+    }
+  }
+`;
+
+const useFilteredData = (inputContent, showByContinent) => {
+  const {
+    data: dataLang,
+    loading: loadingLang,
+    error: errorLang,
+  } = useQuery(LIST_LANGUAGES);
+  const {
+    data: dataCont,
+    loading: loadingCont,
+    error: errorCont,
+  } = useQuery(LIST_CONTINENTS);
+  const { data, loading, error } = useQuery(LIST_COUNTRIES);
+
+  const results = useMemo(() => {
+    if (
+      !loading &&
+      !error &&
+      !loadingCont &&
+      !errorCont &&
+      !loadingLang &&
+      !errorLang
+    ) {
+      const res = inputContent
+        ? data.countries.filter((c) => {
+            return c.name.toLowerCase().startsWith(inputContent.toLowerCase());
+          })
+        : data.countries;
+      if (showByContinent) {
+        return dataCont.continents.map(({ name }) => ({
+          name,
+          countries: res.filter(({ continent }) => continent.name === name),
+        }));
+      } else {
+        return dataLang.languages.map(({ name }) => ({
+          name,
+          countries: res.filter(({ languages }) =>
+            languages.some((lang) => lang.name === name)
+          ),
+        }));
+      }
+    }
+  }, [inputContent, data, dataCont, dataLang]);
+  return { results, loading, error };
+};
+
+export default useFilteredData;
