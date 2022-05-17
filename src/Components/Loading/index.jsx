a52c6e3a8932e883829bcf86e882edd67b3c9cd0
Jonatan Luis Moreno <jonatanluismoreno@gmail.com>
2022-05-17 06:48:30 -0300

    add Loading Component

---
+++ src/Components/Loading/index.jsx
@@ -0,0 +1,91 @@
+import React from "react";
+import styled from "styled-components";
+
+/*
+.lds-ring {
+  display: inline-block;
+  position: relative;
+  width: 80px;
+  height: 80px;
+}
+.lds-ring div {
+  box-sizing: border-box;
+  display: block;
+  position: absolute;
+  width: 64px;
+  height: 64px;
+  margin: 8px;
+  border: 8px solid #fff;
+  border-radius: 50%;
+  animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+  border-color: #fff transparent transparent transparent;
+}
+.lds-ring div:nth-child(1) {
+  animation-delay: -0.45s;
+}
+.lds-ring div:nth-child(2) {
+  animation-delay: -0.3s;
+}
+.lds-ring div:nth-child(3) {
+  animation-delay: -0.15s;
+}
+@keyframes lds-ring {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+*/
+
+const StyledLoading = styled.div`
+  display: flex;
+  width: 80px;
+  height: 80px;
+  margin: 0 auto;
+  padding: 37vw;
+`;
+
+const StyledLoadingDiv = styled.div`
+  box-sizing: border-box;
+  display: block;
+  position: absolute;
+  width: 64px;
+  height: 64px;
+  margin: 8px;
+  border: 8px solid #fff;
+  border-radius: 50%;
+  animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+  border-color: #ddd transparent transparent transparent;
+  :nth-child(1) {
+    animation-delay: -0.45s;
+  }
+  :nth-child(2) {
+    animation-delay: -0.3s;
+  }
+  :nth-child(3) {
+    animation-delay: -0.15s;
+  }
+  @keyframes lds-ring {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+`;
+
+const Loading = () => {
+  return (
+    <StyledLoading>
+      <StyledLoadingDiv />
+      <StyledLoadingDiv />
+      <StyledLoadingDiv />
+      <StyledLoadingDiv />
+    </StyledLoading>
+  );
+};
+
+export default Loading;
