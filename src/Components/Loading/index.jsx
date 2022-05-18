e374d2ab04b9b4b32b82cc7e38f890ec056570db
Jonatan Luis Moreno <jonatanluismoreno@gmail.com>
2022-05-18 06:45:26 -0300

    solving visual issue w/ Loading Component

--- src/Components/Loading/index.jsx
+++ src/Components/Loading/index.jsx
@@ -18,16 +18,16 @@ const StyledLoadingDiv = styled.div`
   margin: 8px;
   border: 8px solid #fff;
   border-radius: 50%;
-  animation: lds-ring 0.7s cubic-bezier(0.5, 0, 0.5, 1) infinite;
-  border-color: #ddd transparent transparent transparent;
-  :nth-child(1) {
-    animation-delay: -0.35s;
+  animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+  border-color: #91735f transparent transparent transparent;
+  .lds-ring div:nth-child(1) {
+    animation-delay: -0.45s;
   }
-  :nth-child(2) {
-    animation-delay: -0.2s;
+  .lds-ring div:nth-child(2) {
+    animation-delay: -0.3s;
   }
-  :nth-child(3) {
-    animation-delay: -0.5s;
+  .lds-ring div:nth-child(3) {
+    animation-delay: -0.15s;
   }
   @keyframes lds-ring {
     0% {
